54623d472b08d69e4863a9f180bd0ae5e8373a4a
ilovin <lovesscott@126.com>
2015-09-23 22:37:42 +0800

    add the .h

---
+++ stdafx.h
@@ -0,0 +1,22 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <opencv2/core/core.hpp>  
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/highgui/highgui.hpp> 
+#include <string>
+
+
+
+
+// TODO:  在此处引用程序需要的其他头文件
