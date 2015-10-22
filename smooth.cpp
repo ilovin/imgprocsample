98197e8547288c0d42e8293cc85d79899f94880b
ilovin <lovesscott@126.com>
2015-10-22 11:48:12 +0800

    the beta version of filter

--- smooth.cpp
+++ smooth.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 using namespace cv;
 
-void peper_salt(Mat&src, double persentage);
+Mat peper_salt(Mat&src, double persentage,float seasonor);
+Mat peper(Mat&src, double persentage);
+Mat salt(Mat&src, double persentage);
 Mat arith_filter(Mat src,int kernal_size);
 Mat geometry_filter(Mat src, int kernal_size);
+Mat harmonic_filter(Mat src, int kernal_size);
 
 int _smooth(string img, string type)
 {
@@ -15,41 +18,87 @@ int _smooth(string img, string type)
 	Mat final_res;
 	w_pic = src.cols;
 	h_pic = src.rows;
-	while (src.rows>1080 / 3 || src.cols>1920 / 3)
+	while (src.rows>1080 / 4 || src.cols>1920 / 4)
 	{
 		w_pic = w_pic * 2 / 3;
 		h_pic = h_pic * 2 / 3;
 		resize(src, src, Size(w_pic, h_pic));
 	}
-	peper_salt(src,0.1);
 	src.convertTo(src, CV_32FC3);
 	src = src / 255;
 	//imshow("src", src);
 	Mat dst;
+	Mat polute_PaS,polute_p,polute_s;
+	final_res = Mat(src.rows * 3, src.cols * 2, CV_32FC3);
+	polute_PaS = peper(src, 0.05);
+	polute_PaS = salt(polute_PaS, 0.05);
+	polute_p = peper(src, 0.1);
+	polute_s = salt(src, 0.1);
+	drawtheblock(polute_PaS, final_res, Point(0, 0), "perper & salt");
+	drawtheblock(polute_s, final_res, Point(0, src.rows), "only salt");
+	drawtheblock(polute_p, final_res, Point(0, src.rows*2), "only peper");
+
+	int tmp = 3;
+	string str0 = "kernal_size = ",str;
+	char c=0;
 	if (type=="-arithmetic"||type=="-a")
 	{
-		final_res = Mat(src.rows, src.cols * 2, CV_32FC3);
-		drawtheblock(src, final_res, Point(0, 0), "peper & salt");
-		int tmp = 3;
-		while (waitKey(500)!=27)
+		while (c!=27)
 		{
-			drawtheblock(arith_filter(src, tmp), final_res, Point(src.cols, 0), "after filter");
-			imshow("display", final_res);
+			char intstr[32];
+			_itoa_s(tmp, intstr,10);
+			c = waitKey(500);
+			str = str0 + string(intstr);
+			drawtheblock(arith_filter(polute_PaS, tmp), final_res, 
+				Point(src.cols, 0), str);
+			drawtheblock(arith_filter(polute_s, tmp), final_res, 
+				Point(src.cols, src.rows), " ");
+			drawtheblock(arith_filter(polute_p, tmp), final_res, 
+				Point(src.cols, src.rows*2), " ");
+
+			imshow("arithmetric", final_res);
+			
+			if (tmp == 3)
+				waitKey(0);
+			if (c == 32)
+				waitKey(0);
+	
 			tmp += 2;
 			if (tmp>src.rows/4||tmp>src.cols/4)
 				break;
 		}
 	}
-	if (type=="-geometry"||type=="-g")
+	else if (type=="-geometry"||type=="-g")
 	{
-		final_res = Mat(src.rows, src.cols * 2, CV_32FC3);
-		drawtheblock(src, final_res, Point(0, 0), "peper & salt");
-		int tmp = 3;
-		drawtheblock(geometry_filter(src, tmp), final_res, Point(src.cols, 0), "after filter");
-		imshow("display", final_res);
+			char intstr[32];
+			_itoa_s(tmp, intstr, 10);
+			str = str0 + string(intstr);
+			drawtheblock(geometry_filter(polute_PaS, tmp), final_res,
+				Point(src.cols, 0), str);
+			drawtheblock(geometry_filter(polute_s, tmp), final_res,
+				Point(src.cols, src.rows), " ");
+			drawtheblock(geometry_filter(polute_p, tmp), final_res,
+				Point(src.cols, src.rows * 2), " ");
 
+			imshow("geometry", final_res);
+			waitKey(0);
 	}
-	waitKey(0);
+	else if (type == "-harmonic" || type == "-h"){
+			char intstr[32];
+			_itoa_s(tmp, intstr, 10);
+			c = waitKey(500);
+			str = str0 + string(intstr);
+			drawtheblock(harmonic_filter(polute_PaS, tmp), final_res,
+				Point(src.cols, 0), str);
+			drawtheblock(harmonic_filter(polute_s, tmp), final_res,
+				Point(src.cols, src.rows), " ");
+			drawtheblock(harmonic_filter(polute_p, tmp), final_res,
+				Point(src.cols, src.rows * 2), " ");
+
+			imshow("harmonic", final_res);
+
+			waitKey(0);
+	}
 
 	return 0;
 }
@@ -71,7 +120,7 @@ Mat geometry_1channal(Mat src, int kernal_size)
 	{
 		for (int j = tmp; j < src.cols+tmp; j++)
 		{
-			float sum = 1;
+			double sum = 1;
 			for (int k = -tmp; k <= tmp; k++)
 			{
 				for (int l = -tmp; l <= tmp; l++)
@@ -80,52 +129,127 @@ Mat geometry_1channal(Mat src, int kernal_size)
 				}
 			}
 			sum = pow(sum, 1.0 / (kernal_size*kernal_size));
-			dst.at<float>(i-tmp, j-tmp) = sum;
+			dst.at<float>(i-tmp, j-tmp) = (float)sum;
 		}
 	}
 	return dst;
-	
 }
-Mat geometry_filter(Mat src, int kernal_size)
-{
+
+Mat harmoni_1channal(Mat src, int kernal_size){
+	Mat mid_mat,dst;
+	dst = Mat(src.size(), src.type());
 	int tmp = (kernal_size - 1) / 2;
+	copyMakeBorder(src, mid_mat, tmp, tmp, tmp, tmp, BORDER_REFLECT_101);
+	for (int i = tmp; i < src.rows+tmp; i++)
+	{
+		for (int j = tmp; j < src.cols+tmp; j++)
+		{
+			double sum = 0;
+			for (int k = -tmp; k <= tmp; k++)
+			{
+				for (int l = -tmp; l <= tmp; l++)
+				{
+					if (mid_mat.at<float>(k + i, l + j) == 0){
+						sum = 0;
+						break;
+					}
+					else
+					{
+						sum += 1.0/mid_mat.at<float>(k+i, l+j);
+					}
+				}
+			}
+			if (sum!=0)
+				sum = 1.0*kernal_size*kernal_size / sum;
+
+			dst.at<float>(i-tmp, j-tmp) = (float)sum;
+		}
+	}
+	return dst;
+}
+
+Mat harmonic_filter(Mat src, int kernal_size){
 	Mat mid_mat,dst;
 	Mat bgr[3];
-	split(src, bgr);
-	bgr[0]=geometry_1channal(bgr[0], kernal_size);
-	bgr[1]=geometry_1channal(bgr[1], kernal_size);
-	bgr[2]=geometry_1channal(bgr[2], kernal_size);
-	merge(bgr,3, dst);
-	return dst;
-
+	if (src.channels()==1)
+	{
+		return harmoni_1channal(src, kernal_size);
+	}
+	else if (src.channels()==3)
+	{
+		split(src, bgr);
+		bgr[0] = harmoni_1channal(bgr[0], kernal_size);
+		bgr[1] = harmoni_1channal(bgr[1], kernal_size);
+		bgr[2] = harmoni_1channal(bgr[2], kernal_size);
+		merge(bgr, 3, dst);
+		return dst;
+	}
+	else
+	{
+		cout << "the channals is invalid for this program" << endl;
+	}
+	return src;
 }
-void peper_salt(Mat&src, double persentage)
+
+Mat geometry_filter(Mat src, int kernal_size)
+{
+	Mat mid_mat,dst;
+	Mat bgr[3];
+	if (src.channels()==1)
+	{
+		return geometry_1channal(src, kernal_size);
+	}
+	else if (src.channels()==3)
+	{
+		split(src, bgr);
+		bgr[0]=geometry_1channal(bgr[0], kernal_size);
+		bgr[1]=geometry_1channal(bgr[1], kernal_size);
+		bgr[2]=geometry_1channal(bgr[2], kernal_size);
+		merge(bgr,3, dst);
+		return dst;
+	}
+	else
+	{
+		cout << "the channals is invalid for this program" << endl;
+	}
+	return src;
+}
+
+
+Mat peper(Mat&src, double persentage)
+{
+	return peper_salt(src, persentage, 0);
+}
+Mat salt(Mat&src, double persentage)
+{
+	return peper_salt(src, persentage, 1);
+}
+
+Mat peper_salt(Mat&src, double persentage,float seasonor)
 {
 	int n = cvRound(persentage*src.rows*src.cols);
-	int x, y,seasonor,chns;
+	int x, y,chns;
 	chns = src.channels();
+	Mat dst = src.clone();
 	for (int k = 0; k < n; k++)
 	{
 		x = rand() % src.rows;
 		y = rand() % src.cols;
-		if (rand() % 2)
-			seasonor = 0;
-		else
-			seasonor = 255;
 		if (chns==1)
 		{
-			src.at<uchar>(x, y) = seasonor;
+			dst.at<float>(x, y) = seasonor;
 		}
 		else
 		{
 			for (int i = 0; i < chns; i++)
 			{
-				src.at<Vec3b>(x, y)[0] = seasonor;
-				src.at<Vec3b>(x, y)[2] = seasonor;
-				src.at<Vec3b>(x, y)[1] = seasonor;
+				dst.at<Vec3f>(x, y)[0] = seasonor;
+				dst.at<Vec3f>(x, y)[2] = seasonor;
+				dst.at<Vec3f>(x, y)[1] = seasonor;
 			}
 
 		} 
 	}
+	return dst;
 
 }
